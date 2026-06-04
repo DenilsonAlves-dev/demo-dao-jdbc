9f473111421c138a3b94abfafe577663e9f95ad5
unknown <ddenilsonalves00@gmail.com>
2026-06-04 11:58:12 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main (String[] args) {
-        Department dp = new Department(1, "books");
-        System.out.println(dp);
+
+        Seller sl = new Seller(1, "Nome", "nome@gmail.com", new Date("10/10/1999"), 1.200, new Department(1, "books"));
+        System.out.println(sl);
+
     }
 }

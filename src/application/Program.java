115d57071c48709f853f8d0a7f7f8c4a9b49a754
unknown <ddenilsonalves00@gmail.com>
2026-06-05 17:16:41 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,11 +9,9 @@ import java.util.Date;
 
 public class Program {
     public static void main (String[] args) {
-
-        Seller sl = new Seller(1, "Nome", "nome@gmail.com", new Date("10/10/1999"), 1.200, new Department(1, "books"));
-        System.out.println(sl);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(1);
+        System.out.println(seller);
         
 
     }

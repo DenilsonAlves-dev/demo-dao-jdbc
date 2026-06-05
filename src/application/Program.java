c6832aa963ef1154af7cf0ad5f6e2c5d600d6591
unknown <ddenilsonalves00@gmail.com>
2026-06-05 15:42:57 -0300

    SellerDaoJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
-import entities.Department;
-import entities.Seller;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
@@ -10,6 +12,9 @@ public class Program {
 
         Seller sl = new Seller(1, "Nome", "nome@gmail.com", new Date("10/10/1999"), 1.200, new Department(1, "books"));
         System.out.println(sl);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        
 
     }
 }
